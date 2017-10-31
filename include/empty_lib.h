c9d9c797d3af16e8f81e4cb941d9e90185979538
domenico.amelino <domenico.amelino@intrinsic-id.com>
2017-10-31 13:53:45 +0100

    added include

---
+++ include/empty_lib.h
@@ -0,0 +1,15 @@
+#ifndef EMPTY_LIB_H
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#include <stdio.h>
+
+bool mgos_empty_print()
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+
+#endif
